e194a2251a3aae3688f1b46b10b576948f38a195
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2023-10-27 09:25:21 -0300

    Update SistemaJogos.java

--- main/java/com/mycompany/sistemajogos/SistemaJogos.java
+++ main/java/com/mycompany/sistemajogos/SistemaJogos.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.sistemajogos;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
 public class SistemaJogos {
 
     public static void main(String[] args) {

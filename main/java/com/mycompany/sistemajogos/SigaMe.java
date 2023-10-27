38c438d703fdcc3aa99fbf7284daee9529f8f263
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2023-10-27 09:25:09 -0300

    Update SigaMe.java

--- main/java/com/mycompany/sistemajogos/SigaMe.java
+++ main/java/com/mycompany/sistemajogos/SigaMe.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.sistemajogos;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
 public class SigaMe {
     Scanner in = new Scanner(System.in);
     int numDigitado;

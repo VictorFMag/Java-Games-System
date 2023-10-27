1846ffdadeb5015e50b1dbeba2a24b91bdaef85e
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2023-10-27 09:24:59 -0300

    Update AdivinheONúmero.java

--- main/java/com/mycompany/sistemajogos/AdivinheONúmero.java
+++ main/java/com/mycompany/sistemajogos/AdivinheONúmero.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.sistemajogos;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
 public class AdivinheONúmero {
     int numGerado = (int)(Math.random() * 100);
     Scanner in = new Scanner(System.in);
